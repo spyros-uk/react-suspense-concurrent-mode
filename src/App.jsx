ab2ddaec95607f5a04919c3e46c349bcbf3eb253
spyros-uk <dev@spyros.uk>
2020-01-12 16:27:12 +0000

    Lifts state to App level

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,46 @@
 import "./styles.css"
-import React, { lazy, Suspense } from "react"
+import React, {
+  lazy,
+  Suspense,
+  useState,
+  useTransition,
+  useDeferredValue
+} from "react"
+import { getPokemonById } from "./pokemonApi"
+import suspensify from "./suspensify"
 import Error from "./Error"
+
 const PokemonDetails = lazy(() => import("./PokemonDetails"))
+const initialPokemonData = suspensify(getPokemonById(1))
 
 export default function App() {
+  const [pokemonData, setPokemonData] = useState(initialPokemonData)
+  const [startTransition] = useTransition()
+  const deferredPokemonData = useDeferredValue(pokemonData, { timeoutMs: 3000 })
+  const isPokemonDataPending = deferredPokemonData !== pokemonData
+
   return (
     <Error>
       <div className="App">
         <h1>Pokemon Catalogue</h1>
         <Suspense fallback={<>Loading...</>}>
-          <PokemonDetails />
+          <PokemonDetails
+            pokemonData={deferredPokemonData}
+            isDataUpdating={isPokemonDataPending}
+          />
+          <button
+            type="button"
+            onClick={() =>
+              startTransition(() =>
+                setPokemonData(
+                  suspensify(getPokemonById(deferredPokemonData.read().id + 1))
+                )
+              )
+            }
+            disabled={isPokemonDataPending}
+          >
+            Next
+          </button>
         </Suspense>
       </div>
     </Error>
--- src/PokemonDetails.jsx
+++ src/PokemonDetails.jsx
@@ -1,30 +1,13 @@
-import React, { useState, useTransition } from "react"
-import { getPokemonById } from "./pokemonApi"
-import suspensify from "./suspensify"
+import React from "react"
 import Spinner from "./Spinner"
 
-const initialPokemon = suspensify(getPokemonById(1))
-
-export default function PokemonDetails() {
-  // return new Error("Test Error")
-  const [pokemonData, setPokemonData] = useState(initialPokemon)
-  const [startTransition, isPendinig] = useTransition({ timeoutMs: 3000 })
-
-  const pokemon = pokemonData.read()
-  const onNext = () =>
-    startTransition(() =>
-      setPokemonData(suspensify(getPokemonById(pokemon.id + 1)))
-    )
-
+export default function PokemonDetails({ pokemonData, isDataUpdating }) {
   return (
     <>
       <div className="InlineContent">
-        <p>Pokemon Name: {pokemon.name}</p>
-        {isPendinig && <Spinner />}
+        <p>Pokemon Name: {pokemonData.read().name}</p>
+        {isDataUpdating && <Spinner />}
       </div>
-      <button type="button" onClick={onNext} disabled={isPendinig}>
-        Next
-      </button>
     </>
   )
 }
