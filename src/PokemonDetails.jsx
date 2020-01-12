5bfcfbea44080decdd326329656b7beb2b9886cf
spyros-uk <dev@spyros.uk>
2020-01-12 15:23:18 +0000

    Adds loading UI compinent while transition is pending

--- src/PokemonDetails.jsx
+++ src/PokemonDetails.jsx
@@ -1,13 +1,14 @@
 import React, { useState, useTransition } from "react"
 import { getPokemonById } from "./pokemonApi"
 import suspensify from "./suspensify"
+import Spinner from "./Spinner"
 
 const initialPokemon = suspensify(getPokemonById(1))
 
 export default function PokemonDetails() {
   // return new Error("Test Error")
   const [pokemonData, setPokemonData] = useState(initialPokemon)
-  const [startTransition, isPendinig] = useTransition({ timeoutMs: 1000 })
+  const [startTransition, isPendinig] = useTransition({ timeoutMs: 3000 })
 
   const pokemon = pokemonData.read()
   const onNext = () =>
@@ -17,7 +18,10 @@ export default function PokemonDetails() {
 
   return (
     <>
-      <p>Pokemon Name: {pokemon.name}</p>
+      <div className="InlineContent">
+        <p>Pokemon Name: {pokemon.name}</p>
+        {isPendinig && <Spinner />}
+      </div>
       <button type="button" onClick={onNext} disabled={isPendinig}>
         Next
       </button>
